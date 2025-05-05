b1d3389d455379e6777d17c788cd22492f0f5c44
VaL <kihwouih@gmail.com>
2025-05-05 15:26:39 +0200

    Ajout du plugin Spotify

--- src/plugins/spotify-plugin.js
+++ src/plugins/spotify-plugin.js
@@ -2,12 +2,6 @@ class SpotifyPlugin {
     constructor() {
         this.id = 'spotify';
         this.name = 'Spotify Plugin';
-        this.client = null; // Spotify API client
-    }
-
-    initialize(config) {
-        // Initialisation du client Spotify
-        // Utiliser les configurations fournies
     }
 
     getActions() {
@@ -15,34 +9,16 @@ class SpotifyPlugin {
             {
                 id: 'play-pause',
                 name: 'Play/Pause',
-                description: 'Toggle play/pause on Spotify',
                 execute: async () => {
-                    if (!this.client) {
-                        throw new Error('Spotify client not initialized');
-                    }
-                    return this.client.playPause();
+                    console.log('Spotify Play/Pause');
                 }
             },
             {
                 id: 'next-track',
                 name: 'Next Track',
-                description: 'Skip to next track',
                 execute: async () => {
-                    if (!this.client) {
-                        throw new Error('Spotify client not initialized');
-                    }
-                    return this.client.nextTrack();
+                    console.log('Spotify Next Track');
                 }
-            }
-        ];
-    }
-
-    getTriggers() {
-        return [
-            {
-                id: 'track-change',
-                name: 'Track Changed',
-                description: 'Triggered when a new track starts playing'
             }
         ];
     }
