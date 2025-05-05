651d15498570bf734e74826848e1019017bca905
VaL <kihwouih@gmail.com>
2025-05-05 15:28:38 +0200

    Mise à jour de la configuration Jest pour les tests unitaires

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,23 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
-  moduleFileExtensions: ['js', 'json', 'node']
-};
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+}
