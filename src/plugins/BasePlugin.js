edc7472f6db821b53df5d3297e1b1a6c4775f3e3
VaL <kihwouih@gmail.com>
2025-05-05 16:24:59 +0200

    Ajout de BasePlugin pour standardiser l'interface des plugins

--- src/plugins/BasePlugin.js
+++ src/plugins/BasePlugin.js
@@ -1,30 +1,21 @@
 class BasePlugin {
-  constructor(name, description) {
-    this.name = name;
-    this.description = description;
-  }
+    constructor() {
+        this.name = this.constructor.name;
+        this.connected = false;
+    }
 
-  // Lifecycle methods
-  async initialize() {
-    // Override in child classes
-    console.log(`Initializing plugin: ${this.name}`);
-  }
+    async initialize(server) {
+        console.log(`Initialisation de base pour ${this.name}`);
+        this.connected = true;
+    }
 
-  async unload() {
-    // Override in child classes
-    console.log(`Unloading plugin: ${this.name}`);
-  }
-
-  // Methods to be implemented by specific plugins
-  registerActions() {
-    // Returns an array of supported actions
-    return [];
-  }
-
-  registerListeners() {
-    // Registers event listeners specific to the plugin
-    return {};
-  }
+    registerActions(actionRegistry) {
+        if (!this.connected) {
+            console.warn(`${this.name} non connecté. Actions non enregistrées.`);
+            return;
+        }
+        console.log(`Aucune action spécifique pour ${this.name}`);
+    }
 }
 
 module.exports = BasePlugin;
