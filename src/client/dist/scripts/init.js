d734115f279a5f898ac67e02f9f6feb0d6757406
VaL <kihwouih@gmail.com>
2025-05-05 17:08:09 +0200

    Créer un fichier séparé pour l'initialisation du service worker

---
+++ src/client/dist/scripts/init.js
@@ -0,0 +1,8 @@
+// Initialize PWA and service worker registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => console.log('SW registered'))
+      .catch(err => console.log('SW registration failed'));
+  });
+}
